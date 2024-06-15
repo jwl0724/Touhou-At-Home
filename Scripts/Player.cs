27ff1ec41bbd4a58fe2fe0b9c73d5db6441475b7
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-15 09:03:24 -0700

    rename private variables and add projectile firing logic

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,12 +5,15 @@ public partial class Player : Area2D {
 	// VARIABLES SECTION
 	[Export]
 	public int Speed { get; set; } = 400;
+	private int Health { get; set; } = 300;
+	private float AttackCD { get; set; } = 0.2f;
+	private float attackTimer = 0;
 	public Vector2 ScreenSize;
 	public static Player Instance { get; private set; }
-	private static Vector2 _defaultScale;
-	private Vector2 _velocity = Vector2.Zero;
-	private AnimatedSprite2D _sprite;
-	private CollisionShape2D _hitbox;
+	private static Vector2 defaultScale;
+	private Vector2 velocity = Vector2.Zero;
+	private AnimatedSprite2D sprite;
+	private CollisionShape2D hitbox;
 
 	// SIGNALS SECTION
 	[Signal]
@@ -23,9 +26,9 @@ public partial class Player : Area2D {
 		}
 		Instance = this;
 		ScreenSize = GetViewportRect().Size;
-		_sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		_hitbox = GetNode<CollisionShape2D>("CollisionShape2D");
-		_defaultScale = Scale;
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		hitbox = GetNode<CollisionShape2D>("CollisionShape2D");
+		defaultScale = Scale;
 		InitializePlayer();
 	}
 
@@ -33,59 +36,66 @@ public partial class Player : Area2D {
 	public override void _Process(double delta) {
 		// process player movement
 		ProcessInput();
-		Position += _velocity * (float) delta;
+		Position += velocity * (float) delta;
 		Position = new Vector2(
 			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
 			y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
 		);
 		ProcessAnimation();
+
+		// process projectile firing
+		if (attackTimer > AttackCD) {
+			// add functionality later
+			GD.Print("Projectile Fired!");
+		} else attackTimer += (float) delta;
 	}
 
 	// SIGNAL HANDLERS SECTION
 	private void OnBodyEntered(Node2D body) {
 		Hide();
 		EmitSignal(SignalName.Hit);
-		_hitbox.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
+		hitbox.SetDeferred(CollisionShape2D.PropertyName.Disabled, true);
 	}
 
 	// METHODS SECTION
+
 	private void InitializePlayer() {
 		Show();
 		Position = new Vector2(x: ScreenSize.X / 2, y: ScreenSize.Y / 2);
-		_hitbox.Disabled = false;
+		hitbox.Disabled = false;
 	}
 
 	private void ProcessAnimation() {
 		// process X
-		if (_velocity.X < 0) {
-			_sprite.Animation = "left";
-			if (_sprite.Rotation > -0.3f) _sprite.Rotate(-0.3f);
-		} else if (_velocity.X > 0) {
-			_sprite.Animation = "right";
-			if (_sprite.Rotation < 0.3f) _sprite.Rotate(0.3f);
+		if (velocity.X < 0) {
+			sprite.Animation = "left";
+			if (sprite.Rotation > -0.3f) sprite.Rotate(-0.3f);
+		} else if (velocity.X > 0) {
+			sprite.Animation = "right";
+			if (sprite.Rotation < 0.3f) sprite.Rotate(0.3f);
 		} else {
-			_sprite.Animation = "default";
-			_sprite.Rotation = 0;
+			sprite.Animation = "default";
+			sprite.Rotation = 0;
 		}
 
 		// process Y
-		if (_velocity.Y != 0) {
-			Scale = new Vector2(_defaultScale.X - 0.1f, _defaultScale.Y + 0.2f);
+		if (velocity.Y != 0) {
+			Scale = new Vector2(defaultScale.X - 0.1f, defaultScale.Y + 0.2f);
 		} else {
-			Scale = _defaultScale;
+			Scale = defaultScale;
 		}
 	}
 
 	private void ProcessInput() {
-		_velocity = Vector2.Zero;
+		velocity = Vector2.Zero;
 
-		if (Input.IsActionPressed("moveRight")) _velocity.X += 1;
-		if (Input.IsActionPressed("moveLeft")) _velocity.X -= 1;
-		if (Input.IsActionPressed("moveUp")) _velocity.Y -= 1;
-		if (Input.IsActionPressed("moveDown")) _velocity.Y += 1;
+		if (Input.IsActionPressed("moveRight")) velocity.X += 1;
+		if (Input.IsActionPressed("moveLeft")) velocity.X -= 1;
+		if (Input.IsActionPressed("moveUp")) velocity.Y -= 1;
+		if (Input.IsActionPressed("moveDown")) velocity.Y += 1;
 
-		if (_velocity.Length() > 0) {
-			_velocity = _velocity.Normalized() * Speed;
+		if (velocity.Length() > 0) {
+			velocity = velocity.Normalized() * Speed;
 		}
 	}
 }
