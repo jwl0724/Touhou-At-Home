444d3427d23555314236ce206f56c1870bf4b582
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-16 10:13:50 -0700

    add movement logic

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -2,49 +2,113 @@ using Godot;
 using System;
 
 public partial class Enemy : RigidBody2D {
+	
 	// VARIABLES SECTION
-	private int health {get; set;}
-	private float attackCD {get; set;}
+	[Signal]
+	public delegate void FireEnemyProjectileEventHandler();
+	public static int EnemyCount {get; private set;} = 0;
+	public int Health {get; set;}
+	public float AttackCD {get; set;}
+	public int Attack {get; set;}
+	public AttackPattern AttackPattern {get; set;}
 	private float attackTimer = 0;
 	private static Vector2 defaultScale;
 	private AnimatedSprite2D sprite;
 	private CollisionShape2D hitbox;
-	private int speed;
-	private Vector2 velocity = Vector2.Zero;
+	private const float stationaryTime = 5f;
+	private float stationaryTimer = 0;
+	private Vector2 leaveVelocity = Vector2.Zero;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
-		sprite = GetNode<AnimatedSprite2D>("AniamtedSprite2D");
+		GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D")
+		.Connect("screen_exited", Callable.From(() => OnVisibleOnScreenNotifier2DScreenExited()));
+		sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 		hitbox = GetNode<CollisionShape2D>("CollisionShape2D");
 		defaultScale = Scale;
+		EnemyCount += 1;
 	}
 
 	private void OnVisibleOnScreenNotifier2DScreenExited() {
+		EnemyCount -= 1;
 		QueueFree();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta) {
 		ProcessAnimation();
+
+		// handle enemy attack
+		if (attackTimer >= AttackCD) {
+			attackTimer = 0;
+			EmitSignal(SignalName.FireEnemyProjectile);
+		} else attackTimer += (float) delta;
+
+		if (stationaryTimer > stationaryTime) {
+			startMovement(delta);
+		} else if (LinearVelocity.IsZeroApprox()) {
+			stationaryTimer += (float) delta;
+		} else {
+			stopMovement(delta);
+		}
 	}
 
 	// METHODS SECTION
+	public void SetRandomVelocity() {
+		Vector2 velocity = new((float) GD.RandRange(150f, 250f), 0);
+		float angle = (float) GD.RandRange(0, Math.PI);
+		LinearVelocity = velocity.Rotated(angle);
+	}
+
+	private void startMovement(double delta) {
+		if (leaveVelocity.IsZeroApprox()) 
+			leaveVelocity = new((float) GD.RandRange(150f, 250f), (float) GD.RandRange(150f, 250f));
+		
+		LinearVelocity = new Vector2(
+			LinearVelocity.X + leaveVelocity.X * 1f * (float) delta, 
+			LinearVelocity.Y + leaveVelocity.Y * 1f * (float) delta
+		);
+	}
+
+	private void stopMovement(double delta) {
+		// slows down enemy till it stops
+		float newVelocityX, newVelocityY, decceleration = 75f * (float) delta;
+
+		// handle X component
+		if (LinearVelocity.X > decceleration) {
+			newVelocityX = LinearVelocity.X - decceleration;
+		} else if (LinearVelocity.X < -decceleration) {
+			newVelocityX = LinearVelocity.X + decceleration;
+		} else {
+			newVelocityX = 0;
+		}
+		// handle Y component
+		if (LinearVelocity.Y > decceleration) {
+			newVelocityY = LinearVelocity.Y - decceleration;
+		} else if (LinearVelocity.Y < -decceleration) {
+			newVelocityY = LinearVelocity.Y + decceleration;
+		} else {
+			newVelocityY = 0;
+		}
+		LinearVelocity = new Vector2(newVelocityX, newVelocityY);
+	}
 
 	private void ProcessAnimation() {
+		
 		// process X
-		if (velocity.X < 0) {
+		if (LinearVelocity.X < -75) {
 			sprite.Animation = "left";
-			if (sprite.Rotation > -0.3f) sprite.Rotate(-0.3f);
-		} else if (velocity.X > 0) {
+			if (sprite.Rotation > -0.2f) sprite.Rotate(-0.2f);
+		} else if (LinearVelocity.X > 75) {
 			sprite.Animation = "right";
-			if (sprite.Rotation < 0.3f) sprite.Rotate(0.3f);
+			if (sprite.Rotation < 0.2f) sprite.Rotate(0.2f);
 		} else {
 			sprite.Animation = "default";
 			sprite.Rotation = 0;
 		}
 
 		// process Y
-		if (velocity.Y != 0) {
+		if (LinearVelocity.Y != 0) {
 			Scale = new Vector2(defaultScale.X - 0.1f, defaultScale.Y + 0.2f);
 		} else {
 			Scale = defaultScale;
