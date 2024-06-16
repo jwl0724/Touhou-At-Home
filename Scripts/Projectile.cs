df7e918cdf898858fc84f3b10e18a99da92f8d56
jwl0724 <122506441+jwl0724@users.noreply.github.com>
2024-06-16 12:21:19 -0700

    fix logic for projectile directions

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -2,23 +2,27 @@ using Godot;
 using System;
 
 public partial class Projectile : CharacterBody2D {
-	public static int projectileCount {get; private set;} = 0;
+	public static int ProjectileCount {get; private set;} = 0;
 	public AnimatedSprite2D Sprite;
 	public int Damage {get; private set;}
 	private float Speed;
 	private float Lifespan;
-	private Vector2 Direction;
+	private float Angle;
 	private float lifeTime = 0;
 
-	public void Start(Vector2 direction, float speed, float lifespan, int damage) {
+	// NOTE: Must be called after instantiating projectile since _Ready doesnt work for some reason
+	public void AssignSprite() {
+		Sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	public void SetProperties(float angle, float speed, float lifespan, int damage) {
 		// connect exit signal to function
 		GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D")
 		.Connect("screen_exited", Callable.From(() => OnVisibleOnScreenNotifier2DScreenExited()));
 		
 		// set projectile properties
-		projectileCount += 1;
-		Sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-		Direction = direction;
+		ProjectileCount += 1;
+		Angle = angle;
 		Speed = speed;
 		Lifespan = lifespan;
 		Damage = damage;
@@ -29,16 +33,16 @@ public partial class Projectile : CharacterBody2D {
 		// handle lifetime
 		if (lifeTime > Lifespan) {
 			QueueFree();
-			projectileCount -= 1;
+			ProjectileCount -= 1;
 		} else lifeTime += (float) delta;
 
-		Velocity = new Vector2(Speed, 0).Rotated(Direction.Angle());
+		Velocity = new Vector2(Speed, 0).Rotated(Angle);
 		KinematicCollision2D collision = MoveAndCollide(Velocity * (float) delta);
 		if (collision != null) QueueFree();
 	}
 
 	private void OnVisibleOnScreenNotifier2DScreenExited() {
 		QueueFree();
-		projectileCount -= 1;
+		ProjectileCount -= 1;
 	}
 }
